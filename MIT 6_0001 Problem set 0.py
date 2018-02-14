a7a46505ca618aba4e2980eef793fadfa4b9944d
Alex Murshak <amurshak@gmail.com>
2018-02-14 14:19:05 -0500

    Update MIT 6_0001 Problem set 0.py

--- MIT 6_0001 Problem set 0.py
+++ MIT 6_0001 Problem set 0.py
@@ -1,5 +1,15 @@
+#MIT 6_0001 Intro to Computer Science Fall 2016 
+#Problem Set 0
+#Assignment:
+#Write a program that does the following in order:
+#1. Asks the user to enter a number “x” 
+#2. Asks the user to enter a number “y”
+#3. Prints out number “x”, raised to the power “y”. 
+#4. Prints out the log (base 2) of “x”.
+#
+#solution by Alex Murshak - 2/14/2018
+
 import numpy as np
-#import math
 
 X = int(input('Please enter a number "x"'))
 Y = int(input('Please enter a number "y"'))
